652e5a5557e85bb5b6d30028e7d96c2a4fbb7752
parth13m <parthmadrewar3@gmail.com>
2025-11-20 13:28:36 +0000

    this is p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    char name[20];
+    int regid;
+    cout<<"My name is : ";
+    cin>>name;
+    cout<<name<<endl;
+    cout<<"My registration id is : ";
+    cin>>regid;
+    cout<<regid;
+    return 0;
+}
